08db546d91ee972ad57f9273b04f2b417860aeba
Matheus Wassermann <matheus5wassermann@gmail.com>
2023-07-02 10:47:14 -0300

    microsservicos java spring cloud e spring boot

---
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/services/PaymentService.java
@@ -0,0 +1,21 @@
+package com.udemy.hrpayroll.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.hrpayroll.entities.Payment;
+import com.udemy.hrpayroll.entities.Worker;
+import com.udemy.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, int days) {
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
